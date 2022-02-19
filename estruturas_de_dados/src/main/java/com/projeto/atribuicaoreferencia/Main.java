0fdffd3d37d2159cfa132594d9c3cde7a31e86a2
Izabella Terêncio <izahterencio@gmail.com>
2022-02-18 21:20:50 -0300

    Teste com cópia de valor de tipos primitivos

--- estruturas_de_dados/src/main/java/com/projeto/atribuicaoreferencia/Main.java
+++ estruturas_de_dados/src/main/java/com/projeto/atribuicaoreferencia/Main.java
@@ -1,2 +1,19 @@
-package com.projeto.atribuicaoreferencia;public class Main {
+package com.projeto.atribuicaoreferencia;
+
+public class Main{
+
+    public static void main(String[] args){
+
+        int intA = 1;
+        int intB = intA;
+
+        System.out.println("Valor de A é igual a: " + intA);
+        System.out.println("Valor de B é igual a: " + intB);
+
+        intA = 2;
+        System.out.println("Novo valor de A é: " + intA);
+        System.out.println("Valor de B é: " + intB);
+
+    }
+
 }
