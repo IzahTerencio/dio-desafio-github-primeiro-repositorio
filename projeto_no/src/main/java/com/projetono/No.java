80f1f7898bb61ad24c7ea017d4aeeadad72f6c6b
Izabella Terêncio <izahterencio@gmail.com>
2022-02-19 18:30:26 -0300

    Criando classe-base Nó

--- projeto_no/src/main/java/com/projetono/No.java
+++ projeto_no/src/main/java/com/projetono/No.java
@@ -1,4 +1,36 @@
 package com.projetono;
 
-public class No {
+public class No{
+
+    private String content;
+    private No nextNode;
+
+    public No(String content){
+        this.nextNode = null; // Garantindo que a referência do nó criado esteja nula
+        this.content = content;
+    }
+
+    // Setters e Getters
+    public String getContent(){
+        return content;
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
+
+    public No getNextNode(){
+        return nextNode;
+    }
+
+    public void setNextNode(No nextNode){
+        this.nextNode = nextNode;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "content='" + content + '\'' +
+                '}';
+    }
 }
