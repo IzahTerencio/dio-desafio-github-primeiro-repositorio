0e28d222539d68b79b01e3c220574690e0ca7d97
Izabella Terêncio <izahterencio@gmail.com>
2022-02-24 19:40:34 -0300

    Implementação do método add() da classe LinkedList

--- listaEncadeada/src/main/java/one/digitalinnovation/LinkedList.java
+++ listaEncadeada/src/main/java/one/digitalinnovation/LinkedList.java
@@ -1,12 +1,31 @@
 package one.digitalinnovation;
 
-public class LinkedList{
+public class LinkedList<T>{
 
     Node<T> firstNodeRef;
 
     // Construtor padrão
     public LinkedList(){
         this.firstNodeRef = null;
+    }
+
+    // Recebendo um conteúdo que deve ser armazenado em um nó, o qual será adicionado à estrutura da lista
+    public void add(T content){
+        Node<T> newNode = new Node<>(content);
+
+        if(this.isEmpty()){
+            firstNodeRef = newNode;
+            return;
+        }
+
+        Node<T> auxNode = firstNodeRef;
+
+        for(int i=0; i<this.size()-1; i++){
+            auxNode = auxNode.getNextNode();
+        }
+
+        auxNode.setNextNode(newNode);
+
     }
 
     // Obtendo o tamanho da estrutura da lista, ou seja, a quantidade de nós presentes nela
