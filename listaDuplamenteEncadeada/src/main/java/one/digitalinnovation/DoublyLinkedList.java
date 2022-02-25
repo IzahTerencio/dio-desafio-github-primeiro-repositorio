10ba6792423628646288c979c56cf258f376270f
Izabella Terêncio <izahterencio@gmail.com>
2022-02-25 18:53:58 -0300

    Implementando os métodos getNode() e get()

--- listaDuplamenteEncadeada/src/main/java/one/digitalinnovation/DoublyLinkedList.java
+++ listaDuplamenteEncadeada/src/main/java/one/digitalinnovation/DoublyLinkedList.java
@@ -13,6 +13,20 @@ public class DoublyLinkedList<T>{
         this.listSize  = 0;
     }
 
+    public T get(int index){
+        return(this.getNode(index).getContent());
+    }
+
+    private DoubleNode<T> getNode(int index){
+        DoubleNode<T> auxNode = firstNode;
+
+        for(int i=0; (i < index) && (auxNode != null); i++){
+            auxNode = auxNode.getNextNode();
+        }
+        return(auxNode);
+
+    }
+
     // Método que retorna o tamanho da estrutura
     public int size(){
         return(listSize);
