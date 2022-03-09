204ab0ba0ceeb613410aadf6d2a621d2515a0415
Izabella Terêncio <izahterencio@gmail.com>
2022-03-09 18:38:43 -0300

    Estudando métodos da Collection List

--- interfaceList/src/main/java/one/digitalinnovation/ExemploList.java
+++ interfaceList/src/main/java/one/digitalinnovation/ExemploList.java
@@ -1,8 +1,6 @@
 package one.digitalinnovation;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ExemploList{
 
@@ -49,6 +47,53 @@ public class ExemploList{
         System.out.println();
         System.out.println("==> IMPRIMINDO ELEMENTOS COM FOR EACH:");
         for(Double nota : notasAlunos) System.out.println(nota);
+
+        System.out.println();
+        System.out.println("Terceira nota adicionada à lista: " + notasAlunos.get(2));
+
+        System.out.println();
+        System.out.println("Menor nota da estrutura: " + Collections.min(notasAlunos));
+
+        System.out.println();
+        System.out.println("Maior nota da estrutura: " + Collections.max(notasAlunos));
+
+        System.out.println();
+        Iterator<Double> iterator = notasAlunos.iterator();
+        double soma = 0.0;
+        while(iterator.hasNext()){
+            double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("Soma das notas dos alunos: " + soma + "\n");
+
+        System.out.println("Media das notas dos alunos: " + (soma / notasAlunos.size()) + "\n");
+
+        notasAlunos.remove(0.0);
+        System.out.println("==> IMPRESSAO 5" + "\n"
+                + notasAlunos.toString());
+        System.out.println();
+
+        notasAlunos.remove(0);
+        System.out.println("==> IMPRESSAO 6" + "\n"
+                + notasAlunos.toString());
+        System.out.println();
+
+        Iterator<Double> it1 = notasAlunos.iterator();
+        while(it1.hasNext()){
+            double next = it1.next();
+            if(next < 7 ){
+                it1.remove();
+            }
+        }
+        System.out.println("==> IMPRESSAO 7" + "\n"
+                + notasAlunos.toString());
+        System.out.println();
+
+        notasAlunos.clear();
+        System.out.println("Lista apagada: " + notasAlunos);
+
+        
+
 
     }
 
